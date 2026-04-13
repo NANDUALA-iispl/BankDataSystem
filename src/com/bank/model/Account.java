464b83a5fcd5c46de90e5bc3cae020f74333e97b
sairamyasree-eng <sai.ramyasree@imageinfosystems.com>
2026-04-13 10:18:01 +0530

    pojo added

--- src/com/bank/model/Account.java
+++ src/com/bank/model/Account.java
@@ -1,5 +1,71 @@
 package com.bank.model;
 
+import java.util.List;
+
 public class Account {
+
+	private String accNo;
+	private String accHolderName;
+	private String accId;
+	private String accType;
+	private Double accBalance;
+	private String accStatus;
+	private List<Transaction> transactionList;
+
+	public Account(String accNo, String accHolderName, String accId, String accType, Double accBalance,
+			String accStatus, List<Transaction> transactionList) {
+
+		this.accNo = accNo;
+		this.accHolderName = accHolderName;
+		this.accId = accId;
+		this.accType = accType;
+		this.accBalance = accBalance;
+		this.accStatus = accStatus;
+		this.transactionList = transactionList;
+	}
+
+	public String getAccNo() {
+		return accNo;
+	}
+
+	public String getAccHolderName() {
+		return accHolderName;
+	}
+
+	public String getAccId() {
+		return accId;
+	}
+
+	public String getAccType() {
+		return accType;
+	}
+
+	public void setAccType(String accType) {
+		this.accType = accType;
+	}
+
+	public Double getAccBalance() {
+		return accBalance;
+	}
+
+	public void setAccBalance(Double accBalance) {
+		this.accBalance = accBalance;
+	}
+
+	public String getAccStatus() {
+		return accStatus;
+	}
+
+	public void setAccStatus(String accStatus) {
+		this.accStatus = accStatus;
+	}
+
+	public List<Transaction> getTransactionList() {
+		return transactionList;
+	}
+
+	public void setTransactionList(List<Transaction> transactionList) {
+		this.transactionList = transactionList;
+	}
 
 }
--- src/com/bank/model/Transaction.java
+++ src/com/bank/model/Transaction.java
@@ -2,4 +2,46 @@ package com.bank.model;
 
 public class Transaction {
 
+	private String tnxId;
+	private Account fromAcc;
+	private Account toAcc;
+	private Double tnxAmount;
+	private String channel;
+	private String tnxStatus;
+
+	public Transaction(String tnxId, Account fromAcc, Account toAcc, Double tnxAmount, String channel,
+			String tnxStatus) {
+
+		this.tnxId = tnxId;
+		this.fromAcc = fromAcc;
+		this.toAcc = toAcc;
+		this.tnxAmount = tnxAmount;
+		this.channel = channel;
+		this.tnxStatus = tnxStatus;
+	}
+
+	public String getTnxId() {
+		return tnxId;
+	}
+
+	public Account getFromAcc() {
+		return fromAcc;
+	}
+
+	public Account getToAcc() {
+		return toAcc;
+	}
+
+	public Double getTnxAmount() {
+		return tnxAmount;
+	}
+
+	public String getChannel() {
+		return channel;
+	}
+
+	public String getTnxStatus() {
+		return tnxStatus;
+	}
+
 }
